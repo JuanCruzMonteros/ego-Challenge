d9769dc18d28f6ce21f43f5937715fa6422ec0f1
Juan Cruz <jjuancruzrey@gmail.com>
2020-06-28 15:24:41 -0300

    Object Models for service

---
+++ src/app/modelObject/modelFeatures.ts
@@ -0,0 +1,26 @@
+export class Model {
+
+    id : number;
+    name : string;
+    segment : string ;
+    year : number ;
+    price : number ;
+    thumbnail : string ;
+    photo : string;
+
+    title : string ;
+    description : string ;
+    model_features : [
+    {
+        name : string ;
+        description : string ;
+        photo : string
+    }
+    ];
+    model_highlights : [
+    {
+        title : string ;
+        content : string ;
+        image : string;
+    }];
+}
